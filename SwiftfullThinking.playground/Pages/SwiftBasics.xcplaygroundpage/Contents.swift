afdc234ac92cd30d6d5999801ae09f821248313d
Fuad Agus Salim <fuadagussalim@mail.ugm.ac.id>
2026-04-18 20:18:16 +0700

    convention

--- SwiftfullThinking.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
+++ SwiftfullThinking.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -6,10 +6,39 @@ var greeting = "Hello, playground"
 
 
 print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
 
-//This is  a command
+
+//print(greeting)
+//print(greeting)
+//print(greeting)
+
+//This is  a comment
 
 //: [Next](@next)
+
+/* Multi-line
+ Comment
+ */
+
+
+//Naming Convention
+//Camel Case - Correct
+//The first word is lowercase and then the first character in every following word is uppercase
+
+
+let firstGreeting = "Hello World"
+let thisIsMyFirstGreeting = "Hello World"
+
+//wrong
+let thisismysecondgreeting = "Hello World"
+
+
+//Pascal Case - Wrong
+let this_is_my_third_greeting = "Hello World"
+
+//This is pascal case
+let ThisIsWhatIsPascalCaseLookLike = "Hello World"
+
+//Camel Case - Right
+let thisIsMyFourthGreeting = "Hello World"
+
