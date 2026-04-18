b1b2bc431dd92705c6cd1ce9c0eaf2db948dc1cf
Fuad Agus Salim <fuadagussalim@mail.ugm.ac.id>
2026-04-18 20:09:10 +0700

    get started

---
+++ SwiftfullThinking.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+//This is  a command
+
+//: [Next](@next)
