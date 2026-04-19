c1972d061f20e91b383fa780a8f07f282b0b3171
Fuad Agus Salim <fuadagussalim@mail.ugm.ac.id>
2026-04-19 09:26:44 +0700

    learn void n non-void function in swift

---
+++ SwiftfullThinking.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//we don't write code to build apps like this
+var myItem : Bool = false
+
+myItem = true
+
+print(myItem)
+
+
+//generally apps working using func
+func myFirstFunction(){
+    print("Hello from my first function n call other functions")
+    mySecondFunction()
+    myThirdFunction()
+}
+
+func mySecondFunction(){
+    print("Hello from my second function")
+}
+
+func myThirdFunction(){
+    print("Hello from my third function")
+}
+
+myFirstFunction()
+//mySecondFunction()
+//myThirdFunction()
+
+//void function
+func getLikesCount(){
+    let likes = 5
+    print(likes)
+}
+
+
+//non void function
+func getUserName() -> String{
+    let userName: String = "Fu"
+    return userName
+}
+
+let name = getUserName()
+
+print(name)
+
+//----------------------
+
+
+
+
+
+
+
+
+
